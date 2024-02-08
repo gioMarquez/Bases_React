594cb1c0a15a79ec461a7f48ee9e1b965f45bb3c
Jhovanny <jhovanny.fragoso@codiceglobal.com>
2024-02-08 16:27:08 -0600

    Initial Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const GifExpertApp = () => {
+  return (
+    <>
+        <h1 className='text-3xl font-bold underline text-emerald-800'>GifExpertApp</h1>
+
+        {/* Input */}
+
+        {/* Listado de Gif */}
+            {/* Gif Item */}
+
+
+
+    </>
+  )
+}
