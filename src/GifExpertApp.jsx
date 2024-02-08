c641a196913b3f7984b9cc83a19c1a6d0a375d5e
Jhovanny <jhovanny.fragoso@codiceglobal.com>
2024-02-08 17:20:41 -0600

    Add a Category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+  //Hooks de react que actualizan el estado
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+    const onAddCategory = () => {
+        setCategories(['SNK',...categories])
+    }
   return (
     <>
-        <h1 className='text-3xl font-bold underline text-emerald-800'>GifExpertApp</h1>
+      <h1 className="text-3xl font-bold text-emerald-800">GifExpertApp</h1>
 
-        {/* Input */}
+      {/* Input */}
 
-        {/* Listado de Gif */}
-            {/* Gif Item */}
-
-
-
+      {/* Listado de Gif */}
+      <button className="bg-green-800 text-white font-bold p-2 " onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category, index) => {
+          return <li key={index}> {category}</li>;
+        })}
+      </ol>
+      {/* Gif Item */}
     </>
-  )
-}
+  );
+};
